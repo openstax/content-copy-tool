60865a2955b4f207f1f8b179ce22deccef5d30eb
Weston Novelli <westonnovelli@Westons-MacBook-Pro.local>
2015-05-21 17:13:49 -0500

    initial commit with some basic proof-of-concept functionality

---
+++ create_workgroup.py
@@ -0,0 +1,71 @@
+# This is a Selenium script that creates a workgroup.
+
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+import datetime
+import json
+import re
+import util as util
+
+class WorkgroupCreator():
+    '''
+    This test...
+        creates a workgroup on the specified server with the specified information
+    '''
+
+    def setup(self):
+        # self.driver = webdriver.Firefox()
+        # chrome driver option
+        self.driver = webdriver.Chrome('/Users/westonnovelli/Documents/textbook-migration/research/selenium-tests/util/chromedriver')
+        self.args = util.parse_input(open('config.json'))
+        self.logger = util.init_logger(self.args['create_workgroup_logger'])
+
+    def teardown(self):
+        util.tearDown(self.driver)
+
+    def create_workgroup(self):
+        # navigate to site
+        server_url = self.args['create_workgroup_on_server']
+        if not re.match('https?://', server_url):
+            self.logger.debug("Prepending \'http://\' to server url.")
+            server_url='http://' + server_url
+        self.driver.get(server_url)
+        self.driver.implicitly_wait(300)
+
+        # login
+        self.logger.info("Logging in.")
+        util.login(self.driver, str(self.args['create_workgroup_with_credentials']))
+        self.driver.implicitly_wait(300)
+
+        # self.driver.get('https://legacydev.cnx.org/GroupWorkspaces/wg2935')
+        # create workgroup
+        self.logger.info("Creating workgroup.")
+        create_link = self.driver.find_element_by_link_text('Create a Workgroup')
+        create_link.click()
+        name = self.driver.find_element_by_name('title')
+        name.clear()
+        name.send_keys(str(self.args['create_workgroup_with_title'])+str(datetime.datetime.now()))
+        create_btn = self.driver.find_element_by_name('form.button.Register')
+        create_btn.click()
+
+        # get ID
+        self.logger.info("Extracting workgroup ID.")
+        url = self.driver.current_url
+        workgroupID = re.search('GroupWorkspaces/wg[0-9]+', url)
+        if workgroupID:
+            workgroupID = re.search('wg[0-9]+', workgroupID.group(0)).group(0)
+        if workgroupID == None:
+            self.logger.error("Workgroup ID is None")
+        else:
+            self.logger.info("Created Workgroup with ID: " + workgroupID)
+            print(workgroupID)
+
+    def run_create_workgroup(self):
+        self.setup()
+        self.create_workgroup()
+        self.teardown()
+
+if __name__ == "__main__":
+    wgc = WorkgroupCreator()
+    wgc.run_create_workgroup()
---
+++ util.py
@@ -0,0 +1,46 @@
+import json
+import logging
+
+def init_logger(filename):
+    """
+    Initializes and returns a basic logger to the specified filename.
+    """
+    logger = logging.getLogger('textbook-migration-logger')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+def login(driver, credentials):
+    """
+    A utility function to log into a cnx site with the specified credentials.
+    Accepts a driver that has been navigated to the home page of a cnx site and
+    enters the credentials and clicks the submit button.
+
+    Arguments:
+        driver - the selenium webdriver running the test.
+        credentials - a string of the username:password to log in with
+
+    Output:
+        None
+    """
+    username = credentials[:str.index(credentials, ':')] # before the colon
+    password = credentials[str.index(credentials, ':')+1:] # after the 1st colon
+    authKey = driver.find_element_by_id('__ac_name')
+    authKey.send_keys(username)
+    pw = driver.find_element_by_id('__ac_password')
+    pw.send_keys(password)
+    signin = driver.find_element_by_name('submit')
+    signin.click()
+
+def parse_input(input):
+    """ Returns the parsed json input """
+    return json.load(input)
+
+def encode_json(data):
+    return json.dumps(json.loads(data), indent=4)
+
+def tearDown(driver):
+    driver.quit()
