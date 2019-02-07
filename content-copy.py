d28240536785e1bb1e87700e48b0213a4e79fda7
tomjw64 <tom.jw64@gmail.com>
2019-02-07 16:43:33 -0600

    Make content-copy.py executable via git

--- content-copy.py
+++ content-copy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
 # Passthrough for backwards compatibility
-print "This command for the CCT is deprecated. Please try the `content-copy` command instead"
+print "This command for the CCT is deprecated. Please try the `content-copy` command instead."
 import content_copy
 content_copy.main()
